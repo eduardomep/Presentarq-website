26cc9eea9dcf148e17e843372ead624d1e3076eb
Your Name <you@example.com>
2020-01-05 11:47:10 -0600

    first commit

---
+++ js/navbar.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var menuButton = document.querySelector(".navbar-toggler");
+var menuItems = document.querySelector(".navbar-collapse");
+var menuIsOpen = false;
+
+
+menuButton.addEventListener("click",function(){
+
+        if(menuIsOpen == false){
+            menuItems.classList.remove("d-none");
+            menuIsOpen = true;
+        }
+        else{
+            menuItems.classList.add("d-none");
+            menuIsOpen = false;
+        }
+    
+});
+
+window.addEventListener("resize", function(){
+    if(window.innerWidth<1200){
+        menuItems.classList.add("d-none");
+        menuIsOpen = false;
+    }
+
+});
+
+
+if(window.innerWidth<1200){
+    menuItems.classList.add("d-none");
+}
+
+
